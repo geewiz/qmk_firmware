7ff38a1c589fbf452e19c9d534ba68628642605c
Jochen Lillich <contact@geewiz.dev>
2021-07-12 12:00:27 +0100

    crkbd:geewiz Extract leader key to user space

---
+++ users/geewiz/leader.c
@@ -0,0 +1,22 @@
+#if defined(LEADER_ENABLE)
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        SEQ_ONE_KEY(KC_S) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_3))); }
+        SEQ_ONE_KEY(KC_A) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_8))); }
+        SEQ_ONE_KEY(KC_O) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_6))); }
+        SEQ_ONE_KEY(KC_U) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_5) SS_TAP(X_KP_2))); }
+        SEQ_TWO_KEYS(KC_A, KC_A) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_9) SS_TAP(X_KP_6))); }
+        SEQ_TWO_KEYS(KC_O, KC_O) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_1) SS_TAP(X_KP_4))); }
+        SEQ_TWO_KEYS(KC_U, KC_U) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_0))); }
+        SEQ_ONE_KEY(KC_S) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_3))); }
+
+        SEQ_ONE_KEY(KC_F) { SEND_STRING(".f7it.net"); }
+    }
+}
+#endif
+
