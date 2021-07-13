115551147df31904f253665f9be54eb8b6d11247
Jochen Lillich <contact@geewiz.dev>
2021-07-13 17:03:55 +0100

    keyboardio/atreus:geewiz Remove home row mods

--- keyboards/keyboardio/atreus/keymaps/geewiz/config.h
+++ keyboards/keyboardio/atreus/keymaps/geewiz/config.h
@@ -1,12 +1,5 @@
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 150
-#define TAPPING_TERM_HOMEROW 250
-// Granular control of the tapping term
-#define TAPPING_TERM_PER_KEY 
-// Allow holding a key as mod after tapping it
-#define TAPPING_FORCE_HOLD_PER_KEY
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
 
 #define COMBO_TERM 50
 #define COMBO_VARIABLE_LEN
--- keyboards/keyboardio/atreus/keymaps/geewiz/keymap.h
+++ keyboards/keyboardio/atreus/keymaps/geewiz/keymap.h
@@ -6,7 +6,6 @@ enum layer_names {
   _DEFAULT,
   _NAV,
   _SYM,
-  _NUM,
   _ADJUST
 };
 
@@ -19,47 +18,14 @@ enum keycodes {
 // Layer switching
 #define LY_NAV MO(_NAV)
 #define LY_SYM MO(_SYM)
-#define LY_NUM MO(_NUM)
 #define LY_ADJ MO(_ADJUST)
 
 // Thumb keys
 #define LTHUMB1 KC_TAB
 #define LTHUMB2 LT(_NAV, KC_BSPC)
-#define LTHUMB3 LT(_NUM, KC_ESC)
+#define LTHUMB3 LALT_T(KC_ESC)
 #define RTHUMB1 KC_ENT
 #define RTHUMB2 LT(_SYM, KC_SPC)
-#define RTHUMB3 KC_F19
+#define RTHUMB3 RGUI_T(KC_DEL)
 
-// Left side home row mods
-#define HM_A LCTL_T(KC_A)
-#define HM_S LALT_T(KC_S)
-#define HM_D LGUI_T(KC_D)
-#define HM_F LSFT_T(KC_F)
-// Right side home row mods
-#define HM_SCLN RCTL_T(KC_SCLN)
-#define HM_L RALT_T(KC_L)
-#define HM_K RGUI_T(KC_K)
-#define HM_J RSFT_T(KC_J)
-// Left side NAV layer mods
-#define F5_CTL LCTL_T(KC_F5)
-#define F6_ALT LALT_T(KC_F6)
-#define F7_GUI LGUI_T(KC_F7)
-#define F8_SFT LSFT_T(KC_F8)
-
-#define DEL_HYP HYPR_T(KC_DEL)
-
-// Abbreviations
-#define OSLSFT OSM(MOD_LSFT)
-#define OSRSFT OSM(MOD_RSFT)
-#define CMDLEFT LCMD(KC_LEFT)
-#define ALTLEFT A(KC_LEFT)
-#define ALTRGHT A(KC_RGHT)
-#define CMDRGHT LCMD(KC_RGHT)
-#define CHATUP A(S(KC_UP))
-#define CHATDN A(S(KC_DOWN))
-#define EURO A(S(KC_2))
-#define CUT LCMD(KC_X)
-#define COPY LCMD(KC_C)
-#define PASTE LCMD(KC_V)
-#define UNDO LCMD(KC_Z)
-#define DELWRD A(KC_BSPC)
+#include "../../../users/geewiz/keysyms.h"
