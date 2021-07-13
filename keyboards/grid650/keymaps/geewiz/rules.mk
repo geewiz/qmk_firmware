1b6a910635209b1535fddd5a90d6669a5efa9203
Jochen Lillich <contact@geewiz.dev>
2021-07-13 19:06:16 +0100

    grid650:geewiz Add leader key support

--- keyboards/grid650/keymaps/geewiz/rules.mk
+++ keyboards/grid650/keymaps/geewiz/rules.mk
@@ -1,17 +1,5 @@
-# MCU name
 MCU = atmega32u4
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
 BOOTLOADER = atmel-dfu
-# BOOTLOADER = halfkay
-
 
 # Processor frequency.
 #     This will define a symbol, F_CPU, in all source code files equal to the
@@ -45,6 +33,10 @@ ARCH = AVR8
 #     CPU clock adjust registers or the clock division fuses), this will be equal to F_CPU.
 F_USB = $(F_CPU)
 
+SRC += matrix.c
+
+QUANTUM_LIB_SRC += i2c_master.c
+
 # Interrupt driven control endpoint task(+60)
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
@@ -54,8 +46,6 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-# MOUSEKEY_ENABLE = no       # Mouse keys
-# EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug test
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
@@ -78,42 +68,5 @@ HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 DEBOUNCE_TYPE = eager_pr
 CUSTOM_MATRIX    = yes
 
-# SRC += analog.c
-
-SRC += matrix.c
-
-	   
-QUANTUM_LIB_SRC += i2c_master.c
-
 VIA_ENABLE = no
-
-
-# BOOTLOADER = halfkay
-
-
-# # Build Options
-# #   comment out to disable the options.
-# #
-# BOOTMAGIC_ENABLE = no  # Virtual DIP switch configuration
-# MOUSEKEY_ENABLE  = yes # Mouse keys
-# EXTRAKEY_ENABLE  = yes # Audio control and System control
-# CONSOLE_ENABLE   = no  # Console for debug
-# COMMAND_ENABLE   = yes # Commands for debug and configuration
-# CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
-# NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-# UNICODE_ENABLE   = yes # Unicode
-# SWAP_HANDS_ENABLE= yes # Allow swapping hands of keyboard
-# SLEEP_LED_ENABLE = no
-# API_SYSEX_ENABLE = no
-# RGBLIGHT_ENABLE = no
-# RGBLIGHT_CUSTOM_DRIVER = no
-
-# RGB_MATRIX_ENABLE = no # enable later
-# DEBOUNCE_TYPE = eager_pr
-
-# # project specific files
-# SRC += matrix.c \
-# 	   analog.c
-# QUANTUM_LIB_SRC += i2c_master.c
-
-# LAYOUTS = default
+LEADER_ENABLE = yes
