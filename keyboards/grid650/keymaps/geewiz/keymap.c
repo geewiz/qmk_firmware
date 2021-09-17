6a7a3828ee04c0474fd994ba0445ddf723e30c16
Jochen Lillich <contact@geewiz.dev>
2021-09-17 10:31:41 +0100

    grid650:geewiz: Dedicated leader key

--- keyboards/grid650/keymaps/geewiz/keymap.c
+++ keyboards/grid650/keymaps/geewiz/keymap.c
@@ -69,14 +69,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP, KC_DEL ,
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT , KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_FN),
-        KC_LGUI, KC_NO  , KC_LALT, KC_NO  , KC_NO  , KC_SPC , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_RALT, KC_NO  , KC_LEFT, KC_DOWN, KC_RGHT,
+        KC_LGUI, KC_NO  , KC_LALT, KC_NO  , KC_NO  , KC_SPC , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_LEAD, KC_NO  , KC_LEFT, KC_DOWN, KC_RGHT,
         KC_F1  , KC_F2  , KC_F3  , KC_F4
     ),
     [_FN] = LAYOUT(
         RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_F13 , KC_DEL , KC_DEL,
         KC_CAPS, KC_NLCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______, _______, KC_HOME, KC_BSPC,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BTN1, KC_BTN2, _______, _______, _______, _______, KC_MS_L, KC_MS_R, _______, KC_LEAD, KC_END ,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_D, KC_LEAD, KC_PGUP, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_D, _______, KC_PGUP, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END ,
         _______, _______, _______, _______        
     )
