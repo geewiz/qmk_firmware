ae74753a7c3692699bbe1e9b92efc58cb1ec3a04
Jochen Lillich <contact@geewiz.dev>
2022-03-24 19:53:12 +0000

    lily58/crkbd/atreus keymaps with improved home row mods

---
+++ keyboards/keyboardio/atreus/keymaps/geewiz/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Special tapping term values for home row mods
+#define TAPPING_TERM_PER_KEY
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+// Allow holding a key as mod (not tap) after tapping it
+#define TAPPING_FORCE_HOLD
+// ...except a few special taps
+#define TAPPING_FORCE_HOLD_PER_KEY
