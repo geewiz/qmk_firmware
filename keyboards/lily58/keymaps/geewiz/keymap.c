0fe0500b1186829ce481e3349b2986f9183aa5f1
Jochen Lillich <contact@geewiz.dev>
2022-04-03 13:26:30 +0100

    lily58:geewiz Change function keys

--- keyboards/lily58/keymaps/geewiz/keymap.c
+++ keyboards/lily58/keymaps/geewiz/keymap.c
@@ -55,13 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
   [_ADJUST] = LAYOUT( \
 /* ,-----------------------------------------------------.                   ,-----------------------------------------------------. */
-     RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , RESET  , \
+     RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET  , \
 /* |--------+--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------+--------| */
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_F9  , KC_F10 , KC_F11 , KC_F11 , XXXXXXX, \
+     XXXXXXX, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
 /* |--------+--------+--------+--------+--------+--------|                   |--------+--------+--------+--------+--------+--------| */
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LY_GAME,                     KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_F12 , XXXXXXX, \
+     XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , XXXXXXX,                     LY_GAME, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
 /* |--------+--------+--------+--------+--------+--------|--------. .--------|--------+--------+--------+--------+--------+--------| */
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RMAC1  ,   RMACS  , XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,\
+     XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , XXXXXXX, RMAC1  ,   RMACS  , XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,\
 /* |--------+--------+--------+--------+--------+--------/-------/   \-------\--------+--------+--------+--------+--------+--------| */
                               _______, _______, _______, _______,       _______, _______, _______, _______ \
 /*                          |--------+--------+--------|--------/     \--------|--------+--------+--------|                          */
