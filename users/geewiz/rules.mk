29ff3e189eda0102c18f209cc56884814cabee13
Jochen Lillich <contact@geewiz.dev>
2022-04-03 14:06:04 +0100

    users/geewiz Set tap/hold globally

--- users/geewiz/rules.mk
+++ users/geewiz/rules.mk
@@ -1,22 +1,23 @@
 SRC += $(USER_PATH)/geewiz.c
 
-BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-COMBO_ENABLE = no          # Key combos
-NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-LEADER_ENABLE = yes         # Leader key
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+COMBO_ENABLE = no           # Key combos
+COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug
 DYNAMIC_MACRO_ENABLE = yes  # Dynamic macros
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+LEADER_ENABLE = yes         # Leader key
+MIDI_ENABLE = no            # MIDI controls
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+UNICODE_ENABLE = no         # Unicode
+
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 ifneq ($(PLATFORM),CHIBIOS)
     ifneq ($(strip $(LTO_SUPPORTED)), no)
