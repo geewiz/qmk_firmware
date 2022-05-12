9064002d4dfd7649ac128492bb92d285f8f5ad16
Jochen Lillich <contact@geewiz.dev>
2022-05-12 12:06:43 +0100

    keyboardio/atreus:geewiz Colemak

--- keyboards/keyboardio/atreus/keymaps/geewiz/rules.mk
+++ keyboards/keyboardio/atreus/keymaps/geewiz/rules.mk
@@ -2,6 +2,5 @@ VPATH += keyboards/gboards/
 
 LTO_ENABLE = yes # Reduces compiled size of firmware
 EXTRAKEY_ENABLE = yes # Enables audio control and System control
-LEADER_ENABLE = yes # Leader key
-AUTO_SHIFT_ENABLE = yes
 COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
