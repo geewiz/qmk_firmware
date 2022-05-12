b26429421f2d4cb307b1fc7270139ac258f3eb85
Jochen Lillich <contact@geewiz.dev>
2022-05-12 12:29:17 +0100

    keyboardio/atreus:geewiz Fix mouse layer

--- keyboards/keyboardio/atreus/keymaps/geewiz/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/geewiz/keymap.c
@@ -84,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MOUSE] = LAYOUT(
 	//,--------------------------------------------.                  ,--------------------------------------------.
-      RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   U_RDO  , U_PST  , U_CPY  , U_CUT  , XXXXXXX,\
+      RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    U_RDO  , U_PST  , U_CPY  , U_CUT  , XXXXXXX,
 	//|--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------|
-      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,\
+      KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------.,--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_ALGR, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,\
+      XXXXXXX, KC_ALGR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
 	//|--------+--------+--------+--------+--------+--------||--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______, _______, _______,  KC_BTN1, KC_BTN3, KC_BTN2, _______, _______, _______
 	//`-----------------------------------------------------'`-----------------------------------------------------'
