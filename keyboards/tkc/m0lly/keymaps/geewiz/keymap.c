6fd59f9f934d001ad0bf2ce6482dd8b6eac70e1f
Jochen Lillich <contact@geewiz.dev>
2022-07-07 21:30:56 +0100

    tkc/m0lly:geewiz Adapt to my layer names

--- keyboards/tkc/m0lly/keymaps/geewiz/keymap.c
+++ keyboards/tkc/m0lly/keymaps/geewiz/keymap.c
@@ -17,11 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "geewiz.h"
 
-enum layer_names {
-    _BASE,
-    _FUNC,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layer
      *
@@ -37,11 +32,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Lead |Win  |Alt  |      Space            |Alt  |Win  |Lead | |   0     | .  |    |
      * `-----------------------------------------------------------' '-------------------'
      */
-    [_BASE] = LAYOUT_all(
+    [_DEFAULT] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV ,     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,              KC_P7,   KC_P8,   KC_P9,   XXXXXXX,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,               KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FUNC),            KC_P1,   KC_P2,   KC_P3,   XXXXXXX,
+        KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, LY_ADJT,              KC_P1,   KC_P2,   KC_P3,   XXXXXXX,
         KC_LEAD, KC_LGUI, KC_LALT,                            KC_SPC,                             XXXXXXX, KC_RALT, KC_RWIN, KC_LEAD,              KC_P0,   XXXXXXX, KC_PDOT, KC_PENT
     ),
 
@@ -59,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                      |     |     |    |    | |         |    |    |
      * `-----------------------------------------------------------' '-------------------'
      */
-    [_FUNC] = LAYOUT_all(
+    [_ADJUST] = LAYOUT_all(
         RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_INS , KC_DEL ,     _______, _______, _______, _______,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP  , _______, _______,              _______, _______, _______, XXXXXXX,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, XXXXXXX, _______,              _______, _______, _______, _______,
