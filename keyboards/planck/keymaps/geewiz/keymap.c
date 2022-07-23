6c49fd0d5228d986cc4c5ed1dfcd5493e60ab5e8
Jochen Lillich <contact@geewiz.dev>
2022-07-23 15:09:06 +0100

    planck:geewiz Add rotary encoder support

--- keyboards/planck/keymaps/geewiz/keymap.c
+++ keyboards/planck/keymaps/geewiz/keymap.c
@@ -8,14 +8,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX, XXXXXXX, KC_J   , KC_L,    KC_U,    KC_Y,    KC_QUOT,
   DH_A,    DH_R,    DH_S,    DH_T,    KC_G,    XXXXXXX, XXXXXXX, KC_M   , DH_N,    DH_E,    DH_I,    DH_O   ,
   KC_Z   , KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, XXXXXXX, KC_K   , KC_H   , KC_COMM, KC_DOT,  KC_SLSH,
-  XXXXXXX, XXXXXXX, ESC_MED, BSP_LWR, KC_NO  , TAB_MSE, KC_NO  , ENT_SYM, SPC_RSE, DEL_ADJ, XXXXXXX, XXXXXXX
+  KC_MUTE, XXXXXXX, ESC_MED, BSP_LWR, KC_NO  , TAB_MSE, KC_NO  , ENT_SYM, SPC_RSE, DEL_ADJ, XXXXXXX, XXXXXXX
 ),
 #else
 [_DEFAULT] = LAYOUT_planck_grid(
   KC_Q   , KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   ,
   DH_A   , DH_S,    DH_D,    DH_F,    KC_G,    _______, _______, KC_H   , DH_J   , DH_K   , DH_L   , DH_QUOT,
   KC_Z   , KC_X   , KC_C,    KC_V,    KC_B,    _______, _______, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,
-  XXXXXXX, XXXXXXX, ESC_MED, BSP_LWR, KC_NO  , TAB_MSE, KC_NO  , ENT_SYM, SPC_RSE, DEL_ADJ, XXXXXXX, XXXXXXX
+  KC_MUTE, XXXXXXX, ESC_MED, BSP_LWR, KC_NO  , TAB_MSE, KC_NO  , ENT_SYM, SPC_RSE, DEL_ADJ, XXXXXXX, XXXXXXX
 ),
 #endif
 
@@ -58,7 +58,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F12 , KC_F7  , KC_F8  , KC_F9  , KC_PSCR, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET  ,
   KC_F11 , KC_F4  , KC_F5  , KC_F6  , KC_SLCK, _______, _______, XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
   KC_F10 , KC_F1  , KC_F2  , KC_F3  , KC_PAUS, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, _______, _______, KC_BSPC, KC_NO  , KC_TAB , KC_NO  , _______, _______, _______, _______, _______
+  KC_MUTE, _______, _______, KC_BSPC, KC_NO  , KC_TAB , KC_NO  , _______, _______, _______, _______, _______
 ),
-
 };
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  switch (get_highest_layer(layer_state)) {
+    case _RAISE:
+      clockwise ? tap_code16(C(KC_TAB)) : tap_code16(S(C(KC_TAB)));
+      break;
+    case _ADJUST:
+      clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
+      break;
+    default:
+      clockwise ? tap_code(KC_PGDN) : tap_code(KC_PGUP);
+      break;
+  }
+  return false;
+}
+#endif
