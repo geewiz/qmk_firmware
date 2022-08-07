1d9e8f1a12b3e6e6c2d2ee9710574289031dfd89
Jochen Lillich <contact@geewiz.dev>
2022-08-07 19:11:32 +0100

    keyboardio/atreus:geewiz Update to new layout

--- keyboards/crkbd/keymaps/geewiz/keymap.c
+++ keyboards/crkbd/keymaps/geewiz/keymap.c
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "keymap.h"
-#include "users/geewiz/features/achordion.h"
 
 extern uint8_t is_master;
 
