8369a29f00693c6e8bd032ffc72a6a594ca8f4d1
Jochen Lillich <contact@geewiz.dev>
2023-01-18 11:46:03 +0000

    fjlabs/polaris:geewiz Fix RALT

--- keyboards/fjlabs/polaris/keymaps/geewiz/keymap.c
+++ keyboards/fjlabs/polaris/keymaps/geewiz/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_GRV ,
         KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, LY_ADJT,
-        XXXXXXX, KC_LGUI, KC_LALT, XXXXXXX, KC_SPC , XXXXXXX, KC_RALT, XXXXXXX, KC_RGUI, XXXXXXX
+        XXXXXXX, KC_LGUI, KC_LALT, XXXXXXX, KC_SPC , XXXXXXX, XXXXXXX, KC_RALT, KC_RGUI, XXXXXXX
     ),
 
     [_GAME] = LAYOUT_all(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, KC_LGUI, XXXXXXX
+        XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, KC_LGUI, XXXXXXX
     ),
 
     [_MOUSE] = LAYOUT_all(
@@ -38,14 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______, _______,
         _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_MS_L, KC_MS_R, _______, KC_DEL ,
         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_WH_R, KC_WH_D, KC_MS_D, _______, _______,
-        XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX
+        XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX
     ),
 
     [_ADJUST] = LAYOUT_all(
         RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUS, KC_PSCR, KC_UP  , _______, _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, TG_GAME, LY_DFLT, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_DEL ,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, TG_GAME, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_DEL ,
         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, KC_DOWN, _______, _______,
-        XXXXXXX, _______, _______, XXXXXXX, LY_MOUS, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX
+        XXXXXXX, _______, _______, XXXXXXX, LY_MOUS, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX
     )
 };
