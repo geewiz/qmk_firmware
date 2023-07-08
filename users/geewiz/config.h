4000464e48b3fa9fa50f21af6aa02afb6d28d7db
Jochen Lillich <contact@geewiz.dev>
2023-07-08 19:43:59 +0100

    users/geewiz: Remove obsolete define

--- users/geewiz/config.h
+++ users/geewiz/config.h
@@ -5,8 +5,6 @@
 #define TAPPING_TERM 180
 // Special tapping term values for home row mods
 #define TAPPING_TERM_PER_KEY
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
 // Allow holding a key as mod (not tap) after tapping it
 #define TAPPING_FORCE_HOLD
 // ...except a few special taps
