0b103a393dc4791e10b1e5b174d3f2fd02a3328c
Jochen Lillich <contact@geewiz.dev>
2023-08-24 09:20:06 +0100

    fjlabs/polaris:geewiz: Fix HHKB layout

--- keyboards/fjlabs/polaris/keymaps/geewiz/keymap.c
+++ keyboards/fjlabs/polaris/keymaps/geewiz/keymap.c
@@ -17,35 +17,35 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "geewiz.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_DEFAULT] = LAYOUT_all(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS,
+    [_DEFAULT] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV ,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
-        ESC_CTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_GRV ,
-        KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, LY_ADJT,
-        XXXXXXX, KC_LGUI, KC_LALT, XXXXXXX, KC_SPC , XXXXXXX, XXXXXXX, KC_RALT, KC_RGUI, XXXXXXX
+        ESC_CTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, LY_ADJT,
+        XXXXXXX, KC_LGUI, KC_LALT,          KC_SPC ,                   KC_RALT, KC_RGUI, XXXXXXX
     ),
 
-    [_GAME] = LAYOUT_all(
+    [_GAME] = LAYOUT_60_tsangan_hhkb(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, KC_LGUI, XXXXXXX
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        XXXXXXX, XXXXXXX, _______,          _______,                   _______, KC_LGUI, XXXXXXX
     ),
 
-    [_MOUSE] = LAYOUT_all(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    [_MOUSE] = LAYOUT_60_tsangan_hhkb(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL ,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_U, _______, _______,
-        _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_MS_L, KC_MS_R, _______, KC_DEL ,
-        _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_WH_R, KC_WH_D, KC_MS_D, _______, _______,
-        XXXXXXX, _______, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX
+        _______, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, KC_WH_L, KC_WH_U, KC_MS_L, KC_MS_R, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_R, KC_WH_D, KC_MS_D, _______, _______,
+        XXXXXXX, _______, _______,          _______,                   _______, _______, XXXXXXX
     ),
 
-    [_ADJUST] = LAYOUT_all(
-        QK_BOOT, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
+    [_ADJUST] = LAYOUT_60_tsangan_hhkb(
+        QK_BOOT, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL ,
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP  , _______, _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, TG_GAME, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______, KC_DEL ,
-        _______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, KC_DOWN, _______, _______,
-        XXXXXXX, _______, _______, XXXXXXX, LY_MOUS, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, TG_GAME, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN, KC_DOWN, _______, _______,
+        XXXXXXX, _______, _______,          LY_MOUS,                   _______, _______, XXXXXXX
     )
 };
