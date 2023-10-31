3ad0b6695c824f8bc0ca4df832d9849a6af0441b
Jochen Lillich <contact@geewiz.dev>
2023-10-31 21:57:12 +0000

    crkbd:geewiz, lulu:geewiz: Make right modifiers one-shots

--- users/geewiz/geewiz.h
+++ users/geewiz/geewiz.h
@@ -73,16 +73,21 @@ enum layer_names {
 #define U_CUT LCTL(KC_X)
 #define U_UND LCTL(KC_Z)
 
-// Abbreviations
-#define CTL_Z LCTL(KC_Z)
-#define CTL_X LCTL(KC_X)
-#define CTL_C LCTL(KC_C)
-#define CTL_V LCTL(KC_V)
+// One-shot modifiers
 #define OS_LSFT OSM(MOD_LSFT)
 #define OS_LCTL OSM(MOD_LCTL)
 #define OS_LALT OSM(MOD_LALT)
 #define OS_LGUI OSM(MOD_LGUI)
 #define OS_RSFT OSM(MOD_RSFT)
+#define OS_RCTL OSM(MOD_RCTL)
+#define OS_RALT OSM(MOD_RALT)
+#define OS_RGUI OSM(MOD_RGUI)
+
+// Abbreviations
+#define CTL_Z LCTL(KC_Z)
+#define CTL_X LCTL(KC_X)
+#define CTL_C LCTL(KC_C)
+#define CTL_V LCTL(KC_V)
 #define DEL_HYP HYPR_T(KC_DEL)
 #define DEL_ALT LALT_T(KC_DEL)
 #define ESC_CTL LCTL_T(KC_ESC)
